003df87b0b89553e6bdb168d85eb11347c5bb39f
ivanfslee <ivanfslee@gmail.com>
2019-11-20 17:49:10 -0800

    Added console logging the html form value

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 let express = require('express');
 
 let app = express();
+app.use(express.urlencoded({extended: false}))
 let path = require('path');
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/create-item', function(req, res) {
+    console.log(req.body.item);
+    res.send("Thank you for submitting the form.")
 });
 
 app.listen(3000);
