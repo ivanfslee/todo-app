bd9e644057e98445cde32840e5b8bc56b771e2a1
ivanfslee <ivanfslee@gmail.com>
2019-11-22 10:45:13 -0800

    Made create todoItem asynchronous

--- public/browser.js
+++ public/browser.js
@@ -1,6 +1,36 @@
+// html for adding a new todo
+function itemTemplate(item) {
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between"> 
+    <span class="item-text">${item.text}</span>
+    <div>
+    <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button> 
+    <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+    </li>` 
+}
+
+// Create todo
+let createField = document.getElementById('create-field');
+
+document.getElementById('create-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    axios.post('/create-item', {text: createField.value}).then(function(response) { //response parameter is the todoItem from backend, sent after adding to database. response is an object 
+        console.log(response);
+        
+        //create html for the new item
+        document.getElementById('item-list').insertAdjacentHTML('beforeend', itemTemplate(response.data));
+        
+        //clear out input field and refocus cursor
+        createField.value = "" 
+        createField.focus(); 
+    }).catch(function() {
+        console.log('Please try again later');
+    }); 
+})
+
+
 document.addEventListener('click', function(e) { 
     console.log('This log is logged to the browser\'s console, not terminal/node.js')
-
     // Delete todo
     if (e.target.classList.contains('delete-me')) {
         if (confirm('Do you really want to delete this item?')) { //confirm is a browser method
