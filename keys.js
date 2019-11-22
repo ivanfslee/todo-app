1d3b4b424fec6370a0dbe790dbc5eec5e0005250
ivanfslee <ivanfslee@gmail.com>
2019-11-21 17:02:02 -0800

    Coded add todo item functionality

--- keys.js
+++ keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://todoAppAdmin:<password>@cluster0-ckghy.mongodb.net/TodoApp?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://todoAppAdmin:59623!!vUv@cluster0-ckghy.mongodb.net/TodoApp?retryWrites=true&w=majority'
 }
